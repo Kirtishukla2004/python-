9283e741ab183636132cf7aa156b7cf41a28b4b2
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2023-01-22 01:53:02 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+
+#using arthmetic operation
+a=int(input("enter first number "))
+b=int(input("enter second number "))
+choice=input("enter what you want to perform +,-,*,/,**,%  ")
+if choice == '+' :
+    print("her's your sum " + str(a+b))
+elif choice == '-':
+      print("her's your subtraction " + str(a-b))
+elif choice == '*':
+      print("her's your multiplication " + str(a*b))
+
+elif choice == '/':
+      print("her's your division " + str(a/b))
+elif choice == '**':
+      print("her's your power solved  " + str(a**b))
+elif choice == '%':
+      print("her's your  modulo " + str(a%b))
+else:
+      print("WRONG INPUTS .....")
