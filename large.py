86858b8cda282f63dfd274eff877fcc3b2bd43cd
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2023-01-22 01:53:29 +0530

    Add files via upload

---
+++ large.py
@@ -0,0 +1,12 @@
+def  checkmax( a, b): 
+    if a>b:
+       print(f'{a} is a maximum number')
+    else :
+        print(f'{b} is a maximum number')
+
+#taking values 
+a=input("enter value of a ")
+b=input("enter value of b")
+checkmax(a,b)
+
+      
