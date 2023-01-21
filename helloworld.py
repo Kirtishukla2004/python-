8478f095e86eabcb6779ba5024e3b7db82af4b8d
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2023-01-22 01:53:15 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+print("hello world00")
+name="somya"
+age=23
+print(name,age)
+name="kirti"
+age=78
+
+name=input("what is your name ")
+print("hello" +name)
+superhero =input("who's your superhero ")
+print(name + "'s superhero is " + superhero)
+a=input("enter first no.")
+b=input("second number ")
+c=int(a) + int(b)
+print( "the sum is " +str(c))
