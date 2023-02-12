206f6388fc8d9714b8e151f18396650ce61fc495
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2023-02-13 02:13:06 +0530

    Add files via upload

---
+++ dictionory.py
@@ -0,0 +1,13 @@
+dict={'riya':18,'shivam':89}
+dict2={'piyush':22,'age':"23"}
+print(dict)
+
+print(len(dict))
+dict.pop('riya')
+print(dict)
+print(dict.get('shivam'))
+print(dict.keys())
+print(dict.values())
+print(str(dict2))
+dict.update({"color": "White"})
+print(dict)
