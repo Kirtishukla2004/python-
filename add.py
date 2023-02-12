76a71bda29846b2c339c9d0a0f165a496196df6e
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2023-02-13 02:12:04 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,7 @@
+def add(a,b): #function function name(argumnets )
+    print("added",a+b) #block of code 
+x= int(input("enter 1st number " ))
+y= int(input("enter 2nd number " ))#statement 
+add(x,y) #function calling 
+import keyword 
+print(keyword,keyword.kwlist)
