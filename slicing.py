6e04c0e649308e9ea11b038a65bbf7d81c015ea0
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2023-02-23 00:15:21 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,28 @@
+str="kirti shukla"
+"""print(str[4:-2])
+print(str[0:1])
+print(str[1:-1:4])
+print(str[:-3])"""
+""""i=1
+for i in str:
+    j=1
+    for j in str:
+        print(i )"""
+      
+"""i=0
+
+while(i<=str[2]):
+    j=0
+    while(j<=5):
+        print(str[j])
+        j=j+1
+    i=i+1"""
+
+#membership in string 
+if('mus' in str):
+    print("fnnjcnj",str)
+elif ('sh' not in str ):
+    print(str)
+else:
+    print("nothing ")
+   
